a0c7993723ad56461c88d045a90e1ffcb55df6fa
birutalukas <biruta.lukas@gmail.com>
2024-10-08 12:11:34 +0300

    add CookieYes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     requestAnimationFrame(raf);
 
+    const mutationsObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            const cookieYesModal = document.querySelector(".cky-modal");
+
+            if (cookieYesModal) {
+                cookieYesModal.setAttribute("data-lenis-prevent", "");
+                mutationsObserver.disconnect(); // Stop observing once the attribute is added
+            }
+        });
+    });
+
+    mutationsObserver.observe(document.body, {
+        childList: true, // Watches for the addition/removal of child nodes
+        subtree: true, // Watches for changes in all descendant nodes
+    });
+
     const revealTitles = document.querySelectorAll(".js-reveal-title");
 
     if (revealTitles) {
